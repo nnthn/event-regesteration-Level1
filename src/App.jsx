51ff15c5c2c2cfb7ae024632da6470001055443f
mutd <ananthan@pop-os.localdomain>
2024-06-22 19:32:43 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventRegistrationForm from './components/EventRegistrationForm';
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <EventRegistrationForm />
+    </div>
+  );
+};
+
+
